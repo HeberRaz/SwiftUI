03857a99d304116a1a38d33e39bb891f5c5aeaed
Heber Raziel Alvarez Ruedas <heber.alvarez@elektra.com.mx>
2023-01-16 12:33:00 -0600

    Uploading WeSplit project with gitignore hidden file

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Heber Raziel Alvarez Ruedas on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
