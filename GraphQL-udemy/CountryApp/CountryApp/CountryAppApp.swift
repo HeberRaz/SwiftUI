9a00f24dba32605c4177cde709d3ef704d8d690a
Hruedas <hruedas@expediagroup.com>
2023-05-23 10:02:09 -0600

    GraphQL: The complete developers guide CountryApp xcode project added

---
+++ GraphQL-udemy/CountryApp/CountryApp/CountryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryAppApp.swift
+//  CountryApp
+//
+//  Created by Heber Alvarez on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CountryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
