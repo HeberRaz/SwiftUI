1d6290289c771df336f60baceafb83cd275cf0ad
Hruedas <hruedas@expediagroup.com>
2023-06-30 11:18:02 -0600

    Add GitHubGraph project from scratch

---
+++ GraphQL-udemy/GitHubGraph/GitHubGraph/GitHubGraphApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubGraphApp.swift
+//  GitHubGraph
+//
+//  Created by Heber Alvarez on 30/06/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubGraphApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
