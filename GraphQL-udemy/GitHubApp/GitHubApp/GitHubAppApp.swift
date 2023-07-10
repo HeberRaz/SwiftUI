6fa002cb5b479c58dd7f406b6893404d25316562
Hruedas <hruedas@expediagroup.com>
2023-07-10 13:18:35 -0600

    Upload github project

---
+++ GraphQL-udemy/GitHubApp/GitHubApp/GitHubAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubAppApp.swift
+//  GitHubApp
+//
+//  Created by Heber Alvarez on 10/07/23.
+//
+
+import SwiftUI
+
+@main
+struct GitHubAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
