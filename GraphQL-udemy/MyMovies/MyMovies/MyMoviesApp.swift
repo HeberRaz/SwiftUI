caa7358e3e33598729606ff30b984a8a25b991a9
Hruedas <hruedas@expediagroup.com>
2023-08-15 13:33:25 -0600

    Add MyMovies app to the folder

---
+++ GraphQL-udemy/MyMovies/MyMovies/MyMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMoviesApp.swift
+//  MyMovies
+//
+//  Created by Heber Alvarez on 15/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MyMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
