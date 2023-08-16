a6b395d04dd7cea631b57dfc8782cb6281405e04
Hruedas <hruedas@expediagroup.com>
2023-08-16 00:05:22 -0600

    Add MoviesApp This files were created from scratch

---
+++ GraphQL-udemy/MoviesApp/MoviesApp/MoviesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesAppApp.swift
+//  MoviesApp
+//
+//  Created by Heber Alvarez on 16/08/23.
+//
+
+import SwiftUI
+
+@main
+struct MoviesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
