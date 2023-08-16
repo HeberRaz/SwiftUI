cdab056cbf86b198304062e3b510bb30017fffa1
Hruedas <hruedas@expediagroup.com>
2023-08-16 00:36:50 -0600

    Add GraphQL folder * Network.swift * Queries.graphql

---
+++ GraphQL-udemy/MoviesApp/MoviesApp/GraphQL/Network.swift
@@ -0,0 +1,16 @@
+//
+//  Network.swift
+//  MoviesApp
+//
+//  Created by Heber Alvarez on 16/08/23.
+//
+
+import Foundation
+
+class Network {
+    static let shared: Network = Network()
+
+    private init() { }
+
+//    lazy var apollo = ApolloClient(url: URL(string: "http://localhost:4000")!)
+}
