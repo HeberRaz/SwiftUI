abd38d00c27e4c6e0999a9cf1a103cee4b53749b
Hruedas <hruedas@expediagroup.com>
2023-08-16 00:43:51 -0600

    Add Views folder

---
+++ GraphQL-udemy/MoviesApp/MoviesApp/Views/FartCounter.swift
@@ -0,0 +1,33 @@
+//
+//  FartCounter1.swift
+//  MoviesApp
+//
+//  Created by Heber Alvarez on 16/08/23.
+//
+
+import SwiftUI
+
+struct FartCounter: View {
+    @State private var fartCount: Int = 0
+
+    var body: some View {
+        VStack {
+            Text("💨")
+                .font(.system(size: 100))
+            Text("\(fartCount)")
+                .font(.largeTitle)
+            Button("Fart") {
+                fartCount += 1
+            }.padding()
+            .background(Color.blue)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+        }
+    }
+}
+
+struct FartCounter_Previews: PreviewProvider {
+    static var previews: some View {
+        FartCounter()
+    }
+}
---
+++ GraphQL-udemy/MoviesApp/MoviesApp/Views/NoResultView.swift
@@ -0,0 +1,28 @@
+//
+//  NoResultView1.swift
+//  MoviesApp
+//
+//  Created by Heber Alvarez on 16/08/23.
+//
+
+import SwiftUI
+
+struct NoResultView: View {
+    let message: String
+
+    var body: some View {
+        HStack {
+            Image(systemName: "exclamationmark.circle")
+                .foregroundColor(.yellow)
+            Text(message)
+        }
+        .padding()
+    }
+}
+
+struct NoResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        NoResultView(message: "No movies found")
+    }
+}
+
