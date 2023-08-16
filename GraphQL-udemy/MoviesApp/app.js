693a00281527de614c8f79eb5d4ccea10f34cd00
Hruedas <hruedas@expediagroup.com>
2023-08-16 00:07:13 -0600

    Setup NodeJS app.js file added data folder added node_modules added package.json file added

---
+++ GraphQL-udemy/MoviesApp/app.js
@@ -0,0 +1,30 @@
+let movies = require('./data/movies')
+const genres = require('./data/genres')
+
+const { ApolloServer, gql } = require('apollo-server')
+
+const typeDefs = gql`
+    type Movie {
+        id: ID!
+        title: String!
+        year: String!
+        genre: String!
+        poster: String!
+    }
+
+    type Query {
+        movies: [Movie]
+    }
+`
+
+const resolvers = {
+    Query: {
+        movies: () => movies
+    }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.listen().then(( { url }) => {
+    console.log(`Server is running at ${url}`)
+})
