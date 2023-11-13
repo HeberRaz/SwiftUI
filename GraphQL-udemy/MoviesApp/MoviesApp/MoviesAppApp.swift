b1c74e63c8e4aebec9fa3d7f014f7e65073bc1a2
Hruedas <hruedas@expediagroup.com>
2023-11-12 22:51:03 -0600

    MoviesApp - Show movies again, I added an expando example to this code, I will keep it here because it is useful but it is not part of Movies app

--- GraphQL-udemy/MoviesApp/MoviesApp/MoviesAppApp.swift
+++ GraphQL-udemy/MoviesApp/MoviesApp/MoviesAppApp.swift
@@ -11,18 +11,12 @@ import SwiftUI
 struct MoviesAppApp: App {
     var body: some Scene {
         WindowGroup {
-            SampleView(
-"""
-Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-"""
-)
+            MovieListScreen()
         }
     }
 }
 
-struct SampleView: View {
+struct ExpandoExample: View {
 
     /* Indicates whether the user want to see all the text or not. */
     @State private var expanded: Bool = false
