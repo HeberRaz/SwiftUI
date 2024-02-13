36364140015c233a28622341f94ffabe018edee2
Hruedas <hruedas@expediagroup.com>
2024-02-13 15:34:41 -0600

    AsyncAwait upload project

---
+++ AsyncAwait/AsyncAwait/AsyncAwaitApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncAwaitApp.swift
+//  AsyncAwait
+//
+//  Created by Heber Alvarez on 13/02/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncAwaitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
